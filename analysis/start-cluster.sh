24d7fcf4b3cdb17808bc043591167f638bf4c18f
AlunYou <xianlun.you@autodesk.com>
2016-04-20 15:42:27 +0800

    modify docker startup script

--- analysis/start-cluster.sh
+++ analysis/start-cluster.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 docker-compose up -d
 
-docker exec -it namenode bash -c 'source /etc/profile.d/hadoop-custom-env.sh && $HADOOP_PREFIX/sbin/start-dfs.sh'
-docker exec -it resourcemanager bash -c 'source /etc/profile.d/hadoop-custom-env.sh && $HADOOP_PREFIX/sbin/start-yarn.sh'
-docker exec -it jobhistory bash -c 'source /etc/profile.d/hadoop-custom-env.sh && $HADOOP_PREFIX/sbin/mr-jobhistory-daemon.sh --config $HADOOP_CONF_DIR start historyserver'
+docker exec -it namenode bash -c 'echo "Y" | $HADOOP_PREFIX/bin/hdfs namenode -format; $HADOOP_PREFIX/sbin/start-dfs.sh; $HADOOP_PREFIX/bin/hdfs dfs -mkdir -p /user/root; '
+docker exec -it resourcemanager bash -c '$HADOOP_PREFIX/sbin/start-yarn.sh'
+docker exec -it jobhistory bash -c '$HADOOP_PREFIX/sbin/mr-jobhistory-daemon.sh --config $HADOOP_CONF_DIR start historyserver'
 
-docker exec -it namenode bash -c 'source /etc/profile.d/hadoop-custom-env.sh && $HADOOP_PREFIX/bin/hdfs dfs -mkdir /user'
-docker exec -it namenode bash -c 'source /etc/profile.d/hadoop-custom-env.sh && $HADOOP_PREFIX/bin/hdfs dfs -mkdir /user/root'
 #docker exec -it namenode bash -c 'source /etc/profile.d/hadoop-custom-env.sh && $HADOOP_PREFIX/bin/hdfs dfs -put $HADOOP_PREFIX/etc/hadoop input'
